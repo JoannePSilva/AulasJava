3b378bc78ff8e550cc4574f24aceef048e652f44
Joanne PSilva <joannepassos@gmail.com>
2021-09-29 15:02:14 -0300

    Impressão

--- src/modulo2/OrientacaoObjetos/ATP15_OO.java
+++ src/modulo2/OrientacaoObjetos/ATP15_OO.java
@@ -14,6 +14,13 @@ public class ATP15_OO {
         ps1.sobrenome = "Ferreira";
         ps1.idade = 25;
 
-        System.out.println();
+        System.out.println("------------------------------");
+        System.out.println(ps.nome + " " + ps.sobrenome);
+        System.out.println("Idade: " + ps.idade);
+        System.out.println("------------------------------");
+        System.out.println(ps1.nome + " " + ps1.sobrenome);
+        System.out.println("idade: " + ps1.idade);
+
+
     }
 }
